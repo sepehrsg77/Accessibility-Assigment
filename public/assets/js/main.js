84ef48568b78c8fdad5230acc665e311abfd0cbc
sepehrsg77 <sepehrsg77@gmail.com>
2024-10-31 20:32:54 -0400

    Delete public directory

--- public/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const checkbox_wrapper = document.querySelector( 'form .flexbox' );
-const checkbox = document.querySelector( 'form .flexbox span' );
-
-checkbox_wrapper.addEventListener("click", function(){
-    checkbox.classList.toggle( "checked" );
-});
